8dbe64f14da2d5d7dd91c60c40fc1cfc8852e080
Anselem <anselemodims@gmail.com>
2022-02-14 14:00:53 +0100

    Feat: add query to select by between weights

--- queries.sql
+++ queries.sql
@@ -7,3 +7,4 @@ SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name!='Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
