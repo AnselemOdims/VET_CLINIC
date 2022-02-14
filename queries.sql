c71f5e72c0c9bc0d28cc4a36f87aac84c9a226ea
Anselem <anselemodims@gmail.com>
2022-02-14 13:51:03 +0100

    Feat: add query to select by date_of_birth

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT * from animals WHERE name like '%mon';
 SELECT * from animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2019';
 SELECT * from animals WHERE neutered=true AND escape_attempts<3;
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
