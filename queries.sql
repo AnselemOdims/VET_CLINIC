cfcf3999eb9018b59c69b8d5cfbed0ce1bde82b9
Anselem <anselemodims@gmail.com>
2022-02-14 13:44:46 +0100

    Feat: add query to select bY neutered and escape_attempts

--- queries.sql
+++ queries.sql
@@ -2,3 +2,4 @@
 
 SELECT * from animals WHERE name like '%mon';
 SELECT * from animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2019';
+SELECT * from animals WHERE neutered=true AND escape_attempts<3;
