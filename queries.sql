e375ee1d8fc5ff64f992accc85a09f3cb587cb41
Anselem <anselemodims@gmail.com>
2022-02-14 13:40:53 +0100

    Feat: add query to select by date between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name like '%mon';
-
+SELECT * from animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2019';
