b359af67ae8d35a8cfe547a57c5cfa3b2b52afe3
Anselem <anselemodims@gmail.com>
2022-02-14 13:56:07 +0100

    Feat: add query to select by neutered

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT * from animals WHERE date_of_birth BETWEEN 'Jan 1, 2016' AND 'Jan 1, 2019
 SELECT * from animals WHERE neutered=true AND escape_attempts<3;
 SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered=true;
