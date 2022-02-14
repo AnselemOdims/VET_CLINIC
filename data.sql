7dda66a30334c13dd0378c8488447b020671f256
Anselem <anselemodims@gmail.com>
2022-02-14 13:22:35 +0100

    Feat: insert Gabumon into animals

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals VALUES(1, 'Agumon', 'Feb 3, 2020',0, true, 10.23);
+INSERT INTO animals VALUES(2, 'Gabumon', 'Nov 15, 2018',2, true, 8.00);
