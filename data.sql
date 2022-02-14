b8469d9039f6d2f4ca12bf8da6be280bb2f33f64
Anselem <anselemodims@gmail.com>
2022-02-14 13:24:52 +0100

    Feat: insert Pikachu into animals

--- data.sql
+++ data.sql
@@ -2,3 +2,4 @@
 
 INSERT INTO animals VALUES(1, 'Agumon', 'Feb 3, 2020',0, true, 10.23);
 INSERT INTO animals VALUES(2, 'Gabumon', 'Nov 15, 2018',2, true, 8.00);
+INSERT INTO animals VALUES(3, 'Pikachu', 'Jan 7, 2021',1, false, 15.04);
