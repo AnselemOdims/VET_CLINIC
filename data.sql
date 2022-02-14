e18d3a17ec98d2362987dc04fef5a4f9f370904b
Anselem <anselemodims@gmail.com>
2022-02-14 13:27:20 +0100

    Feat: insert Devimon into animals

--- data.sql
+++ data.sql
@@ -3,3 +3,4 @@
 INSERT INTO animals VALUES(1, 'Agumon', 'Feb 3, 2020',0, true, 10.23);
 INSERT INTO animals VALUES(2, 'Gabumon', 'Nov 15, 2018',2, true, 8.00);
 INSERT INTO animals VALUES(3, 'Pikachu', 'Jan 7, 2021',1, false, 15.04);
+INSERT INTO animals VALUES(4, 'Devimon', 'May 12, 2017',5, true, 11.00);
