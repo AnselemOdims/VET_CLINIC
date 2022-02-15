b6b86dbcf62ba23cf8a2cfbb31af5f2ae59004cc
Anselem <anselemodims@gmail.com>
2022-02-15 10:17:43 +0100

    Feat: add an entry for Chamander

--- data.sql
+++ data.sql
@@ -1,9 +1,17 @@
 /* Populate the animals table with initial data. */
 INSERT INTO 
-    animals 
-      (name, date_of_birth, escape_attempts, neutered, weight_kg)
-    VALUES
-      ('Agumon', 'Feb 3, 2020',0, true, 10.23),
-      ('Gabumon', 'Nov 15, 2018',2, true, 8.00),
-      ('Pikachu', 'Jan 7, 2021',1, false, 15.04),
-      ('Devimon', 'May 12, 2017',5, true, 11.00);
+  animals 
+    (name, date_of_birth, escape_attempts, neutered, weight_kg)
+  VALUES
+    ('Agumon', 'Feb 3, 2020',0, true, 10.23),
+    ('Gabumon', 'Nov 15, 2018',2, true, 8.00),
+    ('Pikachu', 'Jan 7, 2021',1, false, 15.04),
+    ('Devimon', 'May 12, 2017',5, true, 11.00);
+
+
+/* Populate the animals table with more data */
+INSERT INTO
+  animals
+    (name, date_of_birth, escape_attempts, neutered, weight_kg)
+  VALUES
+    ('Charmander', 'Feb 8, 2020', 0, false, -11),
