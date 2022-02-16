7ab47a5e1b774f617cc2e458d55a188da61ed1a2
Anselem <anselemodims@gmail.com>
2022-02-16 10:52:47 +0100

    Feat: populate the owners table with data

--- data.sql
+++ data.sql
@@ -81,3 +81,15 @@ UPDATE animals
   WHERE weight_kg < 0;
 
 COMMIT TRANSACTION;
+
+/* Populate the owners table with data */
+INSERT INTO 
+  owners
+    (full_name, age)
+  VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
