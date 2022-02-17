4e8b74b455f33ecd1c67babb3b2364f9dc10daa1
Anselem <anselemodims@gmail.com>
2022-02-17 10:33:48 +0100

    Feat: species Maisy Smith got the most

--- queries.sql
+++ queries.sql
@@ -233,3 +233,21 @@ SELECT
     ON sp.vets_id = vs.vets_id
   WHERE sp.id IS NULL
   GROUP BY v.name;
+
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most
+SELECT 
+  v.name AS vet_name,
+  s.name AS species_name,
+  COUNT(s.name)
+  FROM visits vs
+  LEFT JOIN animals a 
+    ON a.id = vs.animal_id
+  LEFT JOIN vets v 
+    ON v.id = vs.vets_id
+  LEFT JOIN species s
+    ON s.id = a.species_id
+  WHERE v.name = 'Maisy Smith'
+  GROUP BY v.name, s.name
+  ORDER BY COUNT DESC
+  LIMIT 1
+;
